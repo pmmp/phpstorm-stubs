94a948d5d1717ba54fcae5fc0bcee43bd7dc6dda
Dylan K. Taylor <dktapps@pmmp.io>
2024-04-02 16:29:01 +0100

    Update ext-encoding stub

--- encoding/encoding.php
+++ encoding/encoding.php
@@ -6,7 +6,44 @@ final class DataDecodeException extends \RuntimeException{
 }
 
 namespace pmmp\encoding {
-final class ByteBuffer{
+abstract class BaseByteBuffer{
+
+	public function __construct(string $buffer = ''){}
+
+	public function toString() : string{}
+
+	public function readByteArray(int $length) : string{}
+
+	public function writeByteArray(string $value) : void{}
+
+	public function getReadOffset() : int{}
+
+	public function getWriteOffset() : int{}
+
+	public function setReadOffset(int $offset) : void{}
+
+	public function setWriteOffset(int $offset) : void{}
+
+	public function getUsedLength() : int{}
+
+	public function getReservedLength() : int{}
+
+	public function reserve(int $length) : void{}
+
+	public function trim() : void{}
+
+	public function clear() : void{}
+
+	public function __serialize() : array{}
+
+	public function __unserialize(array $data) : void{}
+
+	public function __debugInfo() : array{}
+}
+}
+
+namespace pmmp\encoding {
+final class ByteBuffer extends \pmmp\encoding\BaseByteBuffer{
 
 	public function readUnsignedByte() : int{}
 
@@ -103,34 +140,6 @@ final class ByteBuffer{
 	public function writeSignedTriadBE(int $value) : void{}
 
 	public function writeSignedTriadLE(int $value) : void{}
-
-	public function __construct(string $buffer = null){}
-
-	public function toString() : string{}
-
-	public function readByteArray(int $length) : string{}
-
-	public function writeByteArray(string $value) : void{}
-
-	public function getOffset() : int{}
-
-	public function setOffset(int $offset) : void{}
-
-	public function getReserved() : int{}
-
-	public function reserve(int $length) : void{}
-
-	public function trim() : void{}
-
-	public function rewind() : void{}
-
-	public function getUnreadLength() : int{}
-
-	public function __serialize() : array{}
-
-	public function __unserialize(array $data) : void{}
-
-	public function __debugInfo() : array{}
 }
 }
 
