9f725b8edfeb38f7e242e7e942939a05bc47840e
Dylan K. Taylor <dktapps@pmmp.io>
2024-04-02 15:39:10 +0100

    Update xxhash stub

--- xxhash/xxhash.php
+++ xxhash/xxhash.php
@@ -1,5 +1,15 @@
 <?php
 
-function xxhash32(string $data): string {}
+function xxhash32(string $data) : string{}
 
-function xxhash64(string $data): string {}
+function xxhash64(string $data) : string{}
+
+function xxhash3_64bits(string $data) : string{}
+
+function xxhash3_128bits(string $data) : string{}
+
+function xxhash32_int(int $value) : int{}
+
+function xxhash64_int(int $value) : int{}
+
+function xxhash3_64bits_int(int $value) : int{}
